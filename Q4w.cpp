fbb0010c507deb4c7f16e05741730ed2597a6b37
Jyotirmay Bhosale <jyotirmaybhosale0625@gmail.com>
2026-08-11 20:00:57 +0530

    Update and rename 4. Print all odd numbers between 1 and 100 to Q4w.cpp

---
+++ Q4w.cpp
@@ -0,0 +1,17 @@
+
+// Print all odd numbers between 1 and 100.
+#include<iostream>
+using namespace std;
+int main(){
+    int i = 1;
+    while (i<=100)
+    {
+        if (i%2!=0)
+        {
+            cout<<i<<endl;
+        }
+        
+        i++;
+    }
+    return 0;
+}
