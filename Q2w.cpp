d31be7d1d2cdf5a3a09cfd809d7fb5348eb030fc
Jyotirmay Bhosale <jyotirmaybhosale0625@gmail.com>
2026-08-11 18:40:59 +0530

    Update and rename Print numbers from 10 down to 1 in reverse order to Q2w.cpp

---
+++ Q2w.cpp
@@ -0,0 +1,13 @@
+//Print numbers from 10 down to 1 in reverse order
+#include<iostream>
+using namespace std;
+int main(){
+    
+    int i=10;
+    while (i>=1)
+    {
+        cout<<i<<endl;
+        i--;
+    }
+    return 0;
+}
