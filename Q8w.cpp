4c2231389fc0301531ff84791e2c331bce394b01
Jyotirmay Bhosale <jyotirmaybhosale0625@gmail.com>
2026-08-13 19:19:10 +0530

    Update and rename Calculate the sum of all odd numbers from 1 up to n to Q8w.cpp

---
+++ Q8w.cpp
@@ -0,0 +1,20 @@
+// Calculate the sum of all odd numbers from 1 up to n
+#include<iostream>
+using namespace std;
+int main(){
+    int n, i=1;
+    cout<<" enter the n number :";
+    cin>>n;
+    int sum =0;
+    while (i<=n)
+    {
+        if (i%2!=0)
+        {
+            sum=i+sum;
+        }
+        i++;
+    }
+    cout<<sum<<endl;
+    
+    return 0;
+}
